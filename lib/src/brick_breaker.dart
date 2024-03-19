8999d298a910f7ba0a2d95fdb5c84a9c1cdd29e2
frappuccino2316 <frappuccino2316@gmail.com>
2024-03-19 23:58:10 +0900

    操作するやつを作った

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
-import 'dart:math' as math; // Add this import
+import 'dart:math' as math;
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart'; // Add this import
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart'; // And this import
+import 'package:flutter/services.dart'; // And this
 
 import 'components/components.dart';
 import 'config.dart';
 
-class BrickBreaker extends FlameGame {
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents {
+  // Modify this line
   BrickBreaker()
       : super(
           camera: CameraComponent.withFixedResolution(
@@ -16,7 +21,7 @@ class BrickBreaker extends FlameGame {
           ),
         );
 
-  final rand = math.Random(); // Add this variable
+  final rand = math.Random();
   double get width => size.x;
   double get height => size.y;
 
@@ -29,13 +34,31 @@ class BrickBreaker extends FlameGame {
     world.add(PlayArea());
 
     world.add(Ball(
-        // Add from here...
         radius: ballRadius,
         position: size / 2,
         velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
             .normalized()
           ..scale(height / 4)));
 
-    debugMode = true; // To here.
+    world.add(Bat(
+        // Add from here...
+        size: Vector2(batWidth, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95))); // To here
+
+    debugMode = true;
   }
+
+  @override // Add from here...
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+    return KeyEventResult.handled;
+  } // To here
 }
